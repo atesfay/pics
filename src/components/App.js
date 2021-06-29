d7af19c1f86ef2e341a8174e1c9628445f5236ac
Abraham Tesfay <abraham@Abrahams-Mac-mini.local>
2021-06-29 16:11:41 -0700

    Add image lister

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
 
 
 class App extends React.Component {
@@ -8,12 +10,9 @@ class App extends React.Component {
 
     onSearchSubmit = async term => {
         // console.log(term);
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
+        const response = await unsplash.get('/search/photos', {
             params: {
                 query: term
-            },
-            headers: {
-                Authorization: 'Client-ID 9OVvHol2hCXalvmRq3XdrPhjZjpJa5JAG1STvxWGLR8' 
             }
         });
         // .then(response => {
@@ -27,7 +26,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: '30px'}} >
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                Found: {this.state.images.length} images
+                <ImageList images={this.state.images}/>
             </div>
         );
     }
