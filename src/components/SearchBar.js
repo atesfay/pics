8d048ce07ac77deb7e59ef7de4a6074faf376d51
Abraham Tesfay <abraham@Abrahams-Mac-mini.local>
2021-06-29 11:55:41 -0700

    Add controled event examples

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event){
-        console.log(event.target.value);
+    state = {term: ''}; // intial state for controled event
 
-    }
+        // for uncontroled event
+    // onInputChange(event){
+    //     console.log(event.target.value);
+
+    // }
 
     // onInputClick(){
     //     console.log('Input was clicked');
@@ -19,7 +22,17 @@ class SearchBar extends React.Component {
                         <input 
                             type="text" 
                             // onClick={this.onInputClick} 
-                            onChange={this.onInputChange}/>
+                            // onChange={event => console.log(event.target.value)} // alternate way to write event in arrow func
+                            // onChange={e => console.log(e.target.value)} // alternate way to write event in arrow func
+
+                                // for unconroled event 
+                            // onChange={this.onInputChange}
+                           value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value.toUpperCase() })
+                            }
+                           /> 
+
+
                     </div>
                 </form>
 
